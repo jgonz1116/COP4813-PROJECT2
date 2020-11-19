7531855880b3d40824f7a5c80a652a31a383d312
Jason Gonzalez <70523734+jgonz1116@users.noreply.github.com>
2020-11-19 09:53:37 -0500

    Updated the project, now opens page and returns .json file

--- Project2/Project2_Flask/forms.py
+++ Project2/Project2_Flask/forms.py
@@ -1,23 +1,47 @@
+import nltk
+import requests
+from Project2_Flask import app
+from nltk import sent_tokenize
+from nltk import word_tokenize
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, IntegerField, SelectField, RadioField
-import requests
 
-class NewsForm(FlaskForm):
-    format = SelectField('Format', choices = [('shared','shared'),
-                                              ('emailed', 'emailed'),
-                                              ('viewed', 'viewed')])
+from Project2_Flask import main_functions
 
-def generateDataFromAPI():
-    url = "paste_the_api_url_here"
-    my_key = main_functions.read_from_file("JSON_Documents/api_keys.json")
+
+class MovieReviewsForm(FlaskForm):
+    keyword = StringField('keyword')
+    orderBy = SelectField('orderBy', choices=[('by-opening-date', 'Opening date'),
+                                                ('by-publication-date', 'Publication date'),
+                                                ('by-title', 'Title')])
+    critics_Picks = RadioField('critics_Picks', choices=[('Y', 'Yes'),
+                                                         ('No', 'No')])
+
+
+def generateDataFromAPI(keyword, order_By, critics_Picks):
+    my_key_dict = main_functions.read_from_file("Project2_Flask/JSON_Documents/api_keys.json")
     my_key = my_key_dict["my_ny_key"]
-
-    final_url = url + my_key
-    """Maybe you need to provide extra information in the API url"""
-
-    response = requests.get(final_url).json()
-    main_functions.save_to_file(response, "JSON_Documents/response.json")
-    response_dict = main_functions.read_from_file("JSON_Documents/response.json")
-
+    url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" + keyword + "&order=" + order_By + "&critics-pick=" + critics_Picks + "&api-key=" + my_key
+    response = requests.get(url).json()
+    main_functions.save_to_file(response, "Project2_Flask/JSON_Documents/response.json")
+    data_requested = main_functions.read_from_file("Project2_Flask/JSON_Documents/response.json")
+    #data_requested = {}
+    movieTitles = []
+    for i in data_requested["results"]:
+        movieTitles.append(i["display_title"])
+    critics = []
+    for i in data_requested["results"]:
+        critics.append(i["byline"])
+    publicationDates = []
+    for i in data_requested["results"]:
+        publicationDates.append(i["publication_date"])
+    reviewUrls = []
+    for i in data_requested["results"]:
+        for j in data_requested["link"]:
+            reviewUrls.append(j["url"])
     """From the response dictionary, you need to filter the data requested by the user"""
+    data_requested["titles"] = movieTitles
+    data_requested["critics"] = critics
+    data_requested["publicationDates"] = publicationDates
+    data_requested["reviewLinks"] = reviewUrls
     return data_requested
--- Project2/Project2_Flask/routes.py
+++ Project2/Project2_Flask/routes.py
@@ -1,17 +1,19 @@
 from Project2_Flask import app, forms
 from flask import request, render_template
 
-@app_route('/')
-@app.route('/search', methods = ['GET', 'POST'])
-def home:
-    my_form = forms.NewsForm(request.form)
+@app.route('/', methods=['GET', 'POST'])
+def searchReviews():
+    my_form = forms.MovieReviewsForm(request.form)
     if request.method == 'POST':
-        format_ = request.form['format_'] # this variable is assigned to the option selected by the user
+        keyword = request.form['keyword']  # this variable is assigned to the option selected by the user
+        orderBy = request.form['orderBy']
+        critics_Picks = request.form['critics_Picks']
         """TO BE CONTINUED"""
+        test = forms.generateDataFromAPI(keyword, orderBy, critics_Picks)
         # Get the values provided by the user
         # Call the API
         # Generate the requested data
+        response = [keyword]
+    return render_template('search.html', form=my_form)
 
-        return_template('results.html')
-
-    render_template('search.html',form=my_form)
+#@app.route('/searchResults')
